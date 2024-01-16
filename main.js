a643e80c0a65c35bc471aecf4f3ff344208a3b92
alina-khamzatova <alinakhamzatova@gmail.com>
2024-01-16 12:15:13 +0100

    ran build app, added Legend, changed styling of point features, small changes in footer

--- main.js
+++ main.js
@@ -41,150 +41,133 @@ async function loadGeoData() {
       source: vectorSource,
     });
 
-    
-  //   var lowRadiationStyle = new Style({
-  //     image: new CircleStyle({
-  //         radius: 5,
-  //         fill: new Fill({color: 'green'}),
-  //         stroke: new Stroke({color: 'black', width: 1})
-  //     })
-  // });
-  
-  // var highRadiationStyle = new Style({
-  //     image: new CircleStyle({
-  //         radius: 5,
-  //         fill: new Fill({color: 'red'}),
-  //         stroke: new Stroke({color: 'black', width: 1})
-  //     })
-  // });
-  
 
 //styling of the Point Features by their Value 
-      var lessthan008 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: 'rgba(47, 214, 26, 0.7)',
-                  opacity: 0.6                        
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(47, 214, 26, 1)',
-                  width: 1,
-                })
-          } )
-      } );
+var lessthan008 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: 'rgba(238, 248, 175, 0.7)',
+          opacity: 0.6                        
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
 
-      var morethan008 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(188, 248, 113, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(188, 248, 113, 1)',
-                  width: 1,
-                })
-          } )
-      } );
+var morethan008 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(188, 248, 113, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
 
-      var morethan1 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(244, 248, 72, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(244, 248, 72, 1)',
-                  width: 1,
-                })
-          } )
-      } );
-      
-      var morethan2 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(212, 195, 6, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(212, 195, 6, 1)',
-                  width: 1,
-                })
-          } )
-      } );
-      
-      var morethan3 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(245, 166, 35, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(245, 166, 35, 1)',
-                  width: 1,
-                })
-          } )
-      } );
+var morethan1 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(203, 233, 179, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
 
-      var morethan4 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(223, 112, 48, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(223, 112, 48, 1)',
-                  width: 1,
-                })
-          } )
-      } );
-      
-      var morethan5 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {
-                  color: "rgba(209, 72, 13, 0.7)",
-              } ),
-              stroke: new Stroke({
-                  color: 'rgba(209, 72, 13, 1)',
-                  width: 1,
-                })
-          } )
-      } );
-      
-      var morethan6 = new Style( {
-          image: new CircleStyle( {
-              radius: 5,
-              fill: new Fill( {color: "rgba(208, 2, 27, 0.7)",} ),
-              stroke: new Stroke({
-                  color: 'rgba(208, 2, 27, 1)',
-                  width: 1,
-                })
+var morethan2 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(138, 205, 187, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
 
-          } )
-      } );
+var morethan3 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(87, 182, 196, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
 
-  // assigning styles to features depending on their value
-  function radiationStyleFunction(feature) {
-    var radiationValue = feature.get('value');
-    // if ( radiationValue <= 0.08) {
-    //     return [lessthan008];
-    // } else if(1.0 >= radiationValue > 0.08) {
-    //     return[morethan008];
-    if(4 >= radiationValue > 0) {
-        return[morethan1];
-    // } else if(3.0 >= radiationValue > 2.0) {
-    //     return[morethan2];
-    // } else if(4.0 >= radiationValue > 3.0) {
-    //     return[morethan3];
-    } else if(6 >= radiationValue > 4) {
-        return[morethan4];
-    // } else if( 6.0 >= radiationValue > 5.0) {
-    //     return[morethan5];
-    } else {
-        return [morethan6];
-    }
-  }
+var morethan4 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(57, 146, 193, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
+
+var morethan5 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {
+          color: "rgba(19, 46, 133, 0.7)",
+      } ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+  } )
+} );
+
+var morethan6 = new Style( {
+  image: new CircleStyle( {
+      radius: 5,
+      fill: new Fill( {color: "rgba(85, 2, 91, 0.7)",} ),
+      stroke: new Stroke({
+          color: 'rgba(140, 140, 140, 0.5)',
+          width: 0.5,
+        })
+
+  } )
+} );
+
+// assigning styles to features depending on their value
+function radiationStyleFunction(feature) {
+var radiationValue = feature.get('value');
+if (radiationValue < 0.8) {
+return [lessthan008];
+} else if (radiationValue >= 0.8 && radiationValue < 1) {
+return [morethan008];
+} else if (radiationValue >= 1 && radiationValue < 2) {
+return [morethan1];
+} else if (radiationValue >= 2 && radiationValue < 3) {
+return [morethan2];
+} else if (radiationValue >= 3 && radiationValue < 4) {
+return [morethan3];
+} else if (radiationValue >= 4 && radiationValue < 5) {
+return [morethan4];
+} else if (radiationValue >= 5 && radiationValue < 6) {
+return [morethan5];
+} else {
+return [morethan6];
+}
+}
 
 
 
